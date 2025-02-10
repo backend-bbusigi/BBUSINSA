2b5bb63cae5a4301bdd628e11220d10aba3c1c61
anxi01 <tjdals2893@naver.com>
2025-02-11 06:58:18 +0900

    Initial commit

---
+++ src/test/java/spring/bbusinsa/BbusinsaApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.bbusinsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbusinsaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
